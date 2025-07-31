d7112e6ea07146278261e53f7893f8b0d0e9c711
Manzi <manzi5000@hotmail.co.uk>
2025-07-31 21:11:52 +0100

    fixed background

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,10 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#222222] ">
+    <main className="flex min-h-screen flex-col relative">
+      <div className="absolute inset-0 bg-gradient-to-br from-purple-900/30 via-transparent to-blue-900/30 animate-pulse"></div>
       <NavBar />
-      <div className="container mx-auto px-12 py-4 ">
+      <div className="container mx-auto px-12 py-4 relative z-10">
         <TopBar />
       </div>
     </main>
